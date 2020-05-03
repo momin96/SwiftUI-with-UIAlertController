83ffc69e84cfed85e5d819120b02386a8e426903
Nasir <nmomin@fulcrumgt.com>
2020-05-03 15:47:07 +0530

    Project Creation

---
+++ SwiftUI with UIAlertController/SwiftUI with UIAlertController/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI with UIAlertController
+//
+//  Created by Nasir Ahmed Momin on 03/05/20.
+//  Copyright © 2020 Nasir Ahmed Momin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
